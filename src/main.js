1c906c578503f2ecc4824af65ed4e28060eb0838
Astrid Andrea Bolaños Garcia <l.n.andreagarcia@hotmail.com>
2023-06-23 20:50:48 -0600

    Prueba merge

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
+//console.log(example, data);
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
+console.log(data);
+//Paso1. Crear el elemento de encabezado
+const header = document.createElement('header');
+//Agregar texto a la etiqueta
+header.textContent = 'Este es un encabezado creado con JavaScript';
+//Agregar lo anterior al DOM
+document.body.appendChild(header);
+//Asignar classs
+header.classList.add('encabezado');
+console.log(header.classList);
